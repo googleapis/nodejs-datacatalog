23e3ad942628f9fbc648d1332c34a4d6593938b3
yoshi-automation <yoshi-automation@google.com>
2021-04-21 03:09:21 -0700

    docs: reformat comments in PolicyTagManager definition
    PiperOrigin-RevId: 368687296
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Apr 15 11:56:51 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 91eee3d039fbdbadee008393504900287bbc6f43
    Source-Link: https://github.com/googleapis/googleapis/commit/91eee3d039fbdbadee008393504900287bbc6f43

--- src/v1/policy_tag_manager_client.ts
+++ src/v1/policy_tag_manager_client.ts
@@ -857,11 +857,12 @@ export class PolicyTagManagerClient {
     >
   ): void;
   /**
-   * Deletes a policy tag. This method also deletes
-   *   - all of its descendant policy tags, if any,
-   *   - the policies associated with the policy tag and its descendants, and
-   *   - references from BigQuery table schema of the policy tag and its
-   *     descendants.
+   * Deletes a policy tag. This method also deletes:
+   *
+   * * all of its descendant policy tags, if any
+   * * the policies associated with the policy tag and its descendants
+   * * references from BigQuery table schema of the policy tag and its
+   *   descendants.
    *
    * @param {Object} request
    *   The request object that will be sent.
